4ff3098299302ecca51ff46ca71b154128c7e19f
Sai Venkat Muthyala <168863836+saivenkat1919@users.noreply.github.com>
2025-10-24 22:14:16 +0530

    firebase snippet

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyARliP8MMBlVrRFfU9VAkWXW0bdfwxiVC4",
-  authDomain: "expensetracker-5dd2d.firebaseapp.com",
-  projectId: "expensetracker-5dd2d",
-  storageBucket: "expensetracker-5dd2d.firebasestorage.app",
-  messagingSenderId: "769442960686",
-  appId: "1:769442960686:web:5494cb6abff55c3157d6bf"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
